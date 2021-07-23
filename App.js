37638187ec19b6af2a69b89b28cbd80010eeea0b
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-07-23 16:06:56 -0400

    initialised and upto first progress

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+    return (
+        <>
+            <h1>Speed Typer</h1>
+            <textarea value=""/>
+            <h4>Time Remaining: </h4>
+            <button>START</button>
+            <h1>wordcount: </h1>
+        </>
+    )
+}
+
+export default App
